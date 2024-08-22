ad77256bf2f2fae7822662791068a8b2c7e80c11
Mikołaj Cięszczyk <mikolaj.cieszczyk@gmail.com>
2024-08-22 08:27:22 +0200

    finished config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export const content = [
+  "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 ];
 export const theme = {
